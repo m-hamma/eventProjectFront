6fceb548b12586cd6314aee323d4eeaabdcb6431
m-hamma <mohamedhamma445@gmail.com>
2026-09-17 10:48:23 +0200

    feat: add home page, search page and navigation menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { Orders } from './orders/orders';
 import { OrderCreate } from './order-create/order-create';
 import { OrderUpdate } from './order-update/order-update';
+import { Search } from './search/search';
+import { Home } from './home/home';
 
 export const routes: Routes = [
-  { path: '', component: Orders },
+  { path: '', component: Home },
+  { path: 'orders', component: Orders },
+  { path: 'search', component: Search },
   { path: 'create', component: OrderCreate },
-  {
-    path: 'orders/update/:id',
-    component: OrderUpdate,
-  },
+  { path: 'orders/update/:id', component: OrderUpdate },
 ];
---
+++ src/app/search/search.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-search',
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './search.html',
+  styleUrl: './search.css',
+})
+export class Search {}
