fe503b343447a066f20f57f76452601459ae6ceb
m-hamma <mohamedhamma445@gmail.com>
2026-09-23 16:41:19 +0200

    feat: role based menu visibility

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
 
 import { LoginRequest } from '../models/login-request';
@@ -10,6 +11,7 @@ import { AuthResponse } from '../models/login-response';
 })
 export class AuthService {
   private http = inject(HttpClient);
+  private router = inject(Router);
 
   private readonly API_URL = 'http://localhost:8080/api/auth';
 
@@ -17,9 +19,10 @@ export class AuthService {
     return this.http.post<AuthResponse>(`${this.API_URL}/login`, request).pipe(
       tap((response) => {
         localStorage.setItem('token', response.token);
+
         localStorage.setItem('userName', response.userName);
+
         localStorage.setItem('role', response.role);
-        console.log(localStorage.getItem('token'));
       }),
     );
   }
@@ -28,6 +31,8 @@ export class AuthService {
     localStorage.removeItem('token');
     localStorage.removeItem('userName');
     localStorage.removeItem('role');
+
+    this.router.navigate(['/login']);
   }
 
   isAuthenticated(): boolean {
@@ -44,5 +49,32 @@ export class AuthService {
 
   getRole(): string | null {
     return localStorage.getItem('role');
+  }
+
+  startTokenWatcher(): void {
+    setInterval(() => {
+      const token = this.getToken();
+
+      if (!token) {
+        return;
+      }
+
+      try {
+        const payload = JSON.parse(atob(token.split('.')[1]));
+
+        const expiration = payload.exp * 1000;
+
+        if (Date.now() >= expiration) {
+          this.logout();
+        }
+      } catch (error) {
+        console.error('Token JWT invalide', error);
+
+        this.logout();
+      }
+    }, 60000);
+  }
+  hasRole(role: string): boolean {
+    return this.getRole() === role;
   }
 }
